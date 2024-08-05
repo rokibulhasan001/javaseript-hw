f27d078793bd0b00b098e859e7a28295f1fac138
rokibulhasan001 <164499990+rokibulhasan001@users.noreply.github.com>
2024-08-05 23:51:31 +0600

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,51 @@
+/* show tex one*/
+function showText(){
+	document.getElementById('s-text').innerHTML="My name is Rokibul Hasann";
+}
+/* show tex one*/
+function showText8(){
+	document.getElementById('s-text8').innerHTML="My name is Rokibul Hasann";
+}
+/* show tex one*/
+function showText1(){
+	document.getElementById('s-text1').innerHTML="My name is Rokibul Hasann";
+}
+/* show tex one*/
+function showText2(){
+	document.getElementById('s-text2').innerHTML="My name is Rokibul Hasann";
+}
+/* show tex one*/
+function showText3(){
+	document.getElementById('s-text3').innerHTML="My name is Rokibul Hasann";
+}
+/* show tex one*/
+function showText4(){
+	document.getElementById('s-text4').innerHTML="My name is Rokibul Hasann";
+}
+/* show tex one*/
+function showText5(){
+	document.getElementById('s-text5').innerHTML="My name is Rokibul Hasann";
+}
+/* text change*/
+function myTeam(){
+	document.getElementById('demo').innerHTML="bootstrap.bundle.min.js";
+
+}
+/* show tex one*/
+function showText6(){
+	document.getElementById('s-text6').innerHTML="My name is Rokibul Hasann";
+}
+/* show tex one*/
+function showText7(){
+	document.getElementById('s-text7').innerHTML="My name is Rokibul Hasann";
+}
+document.getElementById('demo2').innerHTML=5+6;
+
+
+let x, y, z;
+x=5;
+y=6;
+z= x+y;
+document.getElementById('demo3').innerHTML="The value of z is"+z+".";
+
+
